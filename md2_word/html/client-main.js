c77a2616630e83c93745e69e1cceb96867fd5513
unknown <zipeijiang@umass.edu>
2020-04-24 16:28:39 -0400

    heroku mod

--- md2_word/html/client-main.js
+++ md2_word/html/client-main.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const url = "https://frozen-castle-51130.herokuapp.com/word"; // NOTE NEW URL
+const url = "https://quiet-cove-10572.herokuapp.com/word"; // NOTE NEW URL
 const postdata_1 = require("./postdata");
 function wordCreate() {
     (() => __awaiter(this, void 0, void 0, function* () {
