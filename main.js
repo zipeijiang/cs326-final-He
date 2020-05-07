48d7d337c62026d7113d1949067f12b7ebb8e00d
Haoqin Liang <haoqinliang@gmail.com>
2020-05-07 01:29:16 -0400

    rm usless file

--- main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var database_sql_1 = require("./database_sql");
-var server_sql_1 = require("./server_sql");
-var theDatabase = new database_sql_1.Database('wojhwndc'); // CHANGE THIS
-var theServer = new server_sql_1.Server(theDatabase);
-theServer.listen(process.env.PORT || 8080);
